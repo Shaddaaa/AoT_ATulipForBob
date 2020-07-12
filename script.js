73cb4fea2165dbf9afaa40549ea59d0f26f448a3
Shaddaaa <florian@kaufmann.cloud>
2020-07-12 15:30:02 +0200

    +added a button to the tulip history pages to go to next/prev tulip

---
+++ script.js
@@ -0,0 +1,40 @@
+let ShaddasATulipForBob = {};
+
+ShaddasATulipForBob.setupUI = function() {
+	let cards = document.getElementById("main-container").children[1].children[1];
+	let card = document.createElement("div");
+	card.classList.add("card");
+	let cHeader = document.createElement("div");
+	cHeader.classList.add("card-header");
+
+	let prevButton = document.createElement("button");
+	prevButton.innerHTML = "<- Previous";
+	prevButton.classList.add("btn");
+    prevButton.classList.add("btn-sm");
+    prevButton.classList.add("btn-primary");
+	prevButton.onclick = function() {
+		window.location.href = "https://ageoftrades.com/game/tulips/" + (ShaddasATulipForBob.tulip-1);
+	}
+
+	let nextButton = document.createElement("button");
+	nextButton.innerHTML = "Next ->";
+	nextButton.classList.add("btn");
+    nextButton.classList.add("btn-sm");
+    nextButton.classList.add("btn-primary");
+	nextButton.onclick = function() {
+		window.location.href = "https://ageoftrades.com/game/tulips/" + (ShaddasATulipForBob.tulip+1);
+	}
+	nextButton.style.float= "right";
+
+
+	cHeader.appendChild(prevButton);
+	cHeader.appendChild(nextButton);
+	card.appendChild(cHeader);
+	cards.insertBefore(card, cards.children[0]);
+}
+
+ShaddasATulipForBob.tulip = window.location.href.split("https://ageoftrades.com/game/tulips/")[1];
+if (ShaddasATulipForBob.tulip!=null) {
+	ShaddasATulipForBob.tulip = Number.parseInt(ShaddasATulipForBob.tulip);
+	ShaddasATulipForBob.setupUI();
+}
